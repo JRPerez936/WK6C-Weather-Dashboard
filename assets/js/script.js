be81a67f0e91d8117119ef48166a6b8e43e5255d
Justin Perez <perezjrp936@gmail.com>
2021-07-28 22:34:13 -0400

    added 5 day forecast feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,6 +33,7 @@ var getLatLon = function(city){
           response.json().then(function(data) {
               console.log(data);
             getTodaysWeather(data, city);
+            get5DWeather(city);
           });
         } else {
           alert("Error: " + response.statusText);
@@ -52,7 +53,6 @@ var getTodaysWeather = function(weather, location){
     .then(function(response) {
         // request was successful
         if (response.ok) {
-          console.log(response);
           response.json().then(function(data) {
             displayTodaysWeather(data, location);
           });
@@ -70,7 +70,6 @@ var displayTodaysWeather = function(weather, location){
     if(weather.length === 0){
         cityWeatherEl.textContent = "No data found.";
     }
-    console.log(weather);
     //initialize variables where info will be shared
     var cityEl = document.createElement("h1");
     var tempEl = document.createElement("h3");
@@ -82,7 +81,7 @@ var displayTodaysWeather = function(weather, location){
     cityEl.textContent = location;
     tempEl.textContent ="Temp: "+weather.current.temp+ "°F";
     windEl.textContent ="Wind: "+weather.current.wind_speed+ " MPH";
-    humidEl.textContent ="Humidity: "+weather.current.humidity;
+    humidEl.textContent ="Humidity: "+weather.current.humidity + "%";
     uvEl.textContent ="UV Index: "+ weather.current.uvi;
 
     cityWeatherEl.innerHTML= "";
@@ -93,6 +92,56 @@ var displayTodaysWeather = function(weather, location){
     cityWeatherEl.appendChild(windEl);
     cityWeatherEl.appendChild(humidEl);
     cityWeatherEl.appendChild(uvEl);
-}
+};
 
+var get5DWeather = function(city){
+        //format openWeather api url
+        var apiUrl3 = "http://api.openweathermap.org/data/2.5/forecast?q="+city+"&units=imperial&appid=4cbd25328987295e23b007fb7a00499b";
+
+        //make a get request to url
+        fetch(apiUrl3)
+        .then(function(response) {
+            // request was successful
+            if (response.ok) {
+              response.json().then(function(data) {
+                  display5DWeather(data);
+              });
+            } else {
+              alert("Error: " + response.statusText);
+            }
+          })
+          .catch(function(error) {
+            alert("Unable to connect to OpenWeather");
+          });
+};
+
+var display5DWeather = function(weather){
+    //check if api returned info
+    if(weather.length === 0){
+        city5DEl.textContent = "No data found.";
+    }
+    console.log(weather);
+    city5DEl.innerHTML = "";
+    
+    for(var i = 0; i<=5; i++){
+        var date= document.createElement("p");
+        var temp= document.createElement("p");
+        var wind= document.createElement("p");
+        var humidity= document.createElement("p");
+
+        date.textContent = weather.list[i].dt;
+        temp.textContent = "Temp: "+ weather.list[i].main.temp + "°F";
+        wind.textContent = "Wind: "+ weather.list[i].wind.speed + " MPH";
+        humidity.textContent = "Humidity: "+ weather.list[i].main.humidity + "%";
+
+        var day5El= document.createElement("div");
+        day5El.classList = "col-sm-2";
+        day5El.appendChild(date);
+        day5El.appendChild(temp);
+        day5El.appendChild(wind);
+        day5El.appendChild(humidity);
+
+        city5DEl.appendChild(day5El);
+    }
+};
 userFormEl.addEventListener("submit", formSubmitHandler);
